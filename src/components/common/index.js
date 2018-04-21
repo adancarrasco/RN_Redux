187a5ed55bef37fe71e7c82f4e209b314ed5371d
Adán Carrasco <adan.carrasco@hotmail.com>
2018-04-21 16:35:15 -0600

    Add common components.

---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+// This imports and exports the Button component
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
